95045ac22469b7c07298f1d234feca6b1b45e335
Viktoriya Atavova <viktoriy.ekimovskaya@gmail.com>
2024-03-18 22:51:39 +0300

    1st part HW commit

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,11 @@
+import urllib.parse
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
+
 
 
 def index(request):
@@ -8,12 +14,19 @@ def index(request):
 
 def bus_stations(request):
     current_page = 1
-    next_page_url = 'write your url'
+    bus_stations_info = []
+    with open(settings.BUS_STATION_CSV, encoding='cp1251') as text:
+        reader = csv.DictReader(text)
+        for row in reader:
+            bus_stations_info.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
+    bus_stations_pagi = Paginator(bus_stations_info, 10)
+    page_number = int(request.GET.get('page', 1))
+    page = bus_stations_pagi.get_page(page_number)
+    next_page_url = f'{reverse(bus_stations)}?{urllib.parse.urlencode({"page": page_number + 1})}'
     return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+            'bus_stations': page,
+            'current_page': current_page,
+            'prev_page_url': None,
+            'next_page_url': next_page_url,
+        })
 
